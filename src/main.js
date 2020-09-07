4ae7dcf74168eb4b10d4d51fd3cb44c20621b345
12tanais <otpusk.kiev@gmail.com>
2020-09-07 07:16:33 +0200

    added navbar and sidebar

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import 'materialize-css/dist/js/materialize.min'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import 'materialize-css/dist/js/materialize';
+import dateFilter from '@/filter/date.filter';
+
 
 Vue.config.productionTip = false
+Vue.filter('dateFilter', dateFilter);
 
 new Vue({
     router,
--- src/router/index.js
+++ src/router/index.js
@@ -4,67 +4,85 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
-        path: '/',
-        name: 'Home',
-        meta: {layout: 'main'},
-        component: () => 
-         import('../views/Home.vue')
-    }, {
-        path: '/login',
-        name: 'Login',
-        meta: { layout: 'empty' },
-        component: () =>
-            import ('../views/Login.vue')
-    }, {
-        path: '/register',
-        name: 'Register',
-        meta: { layout: 'empty' },
-        component: () =>
-            import ('../views/Register.vue')
-    }, {
-        path: '/detal',
-        name: 'DetalRecord',
-        meta: { layout: 'main' },
-        component: () =>
-            import ('../views/DetalRecord.vue')
-    }, {
-        path: '/categories',
-        name: 'Categories',
-        meta: { layout: 'main' },
-        component: () =>
-            import ('../views/Categories.vue')
-    }, {
-        path: '/record',
-        name: 'Record',
-        meta: { layout: 'main' },
-        component: () =>
-            import ('../views/Record.vue')
-    }, {
-        path: '/history',
-        name: 'History',
-        meta: { layout: 'main' },
-        component: () =>
-            import ('../views/History.vue')
-    }, {
-        path: '/planing',
-        name: 'Planing',
-        meta: { layout: 'main' },
-        component: () =>
-            import ('../views/Planning.vue')
-    }, {
-        path: '/profile',
-        name: 'Profile',
-        meta: { layout: 'main' },
-        component: () =>
-            import ('../views/Profile.vue')
-    }
+    path: '/',
+    name: 'Home',
+    meta: {
+      layout: 'main'
+    },
+    component: () =>
+      import('../views/Home.vue')
+  }, {
+    path: '/login',
+    name: 'Login',
+    meta: {
+      layout: 'empty'
+    },
+    component: () =>
+      import('../views/Login.vue')
+  }, {
+    path: '/register',
+    name: 'Register',
+    meta: {
+      layout: 'empty'
+    },
+    component: () =>
+      import('../views/Register.vue')
+  }, {
+    path: '/detal',
+    name: 'DetalRecord',
+    meta: {
+      layout: 'main'
+    },
+    component: () =>
+      import('../views/DetalRecord.vue')
+  }, {
+    path: '/categories',
+    name: 'Categories',
+    meta: {
+      layout: 'main'
+    },
+    component: () =>
+      import('../views/Categories.vue')
+  }, {
+    path: '/record',
+    name: 'Record',
+    meta: {
+      layout: 'main'
+    },
+    component: () =>
+      import('../views/Record.vue')
+  }, {
+    path: '/history',
+    name: 'History',
+    meta: {
+      layout: 'main'
+    },
+    component: () =>
+      import('../views/History.vue')
+  }, {
+    path: '/planing',
+    name: 'Planing',
+    meta: {
+      layout: 'main'
+    },
+    component: () =>
+      import('../views/Planning.vue')
+  }, {
+    path: '/profile',
+    name: 'Profile',
+    meta: {
+      layout: 'main'
+    },
+    component: () =>
+      import('../views/Profile.vue')
+  }
 
 ]
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
 
 export default router
