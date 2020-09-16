7e7e2a910bc1ad361b9e84a36af4da2b89382c80
12Aleks <tanais2010@yahoo.com>
2020-09-16 20:43:01 +0200

    added new categories component

---
+++ src/components/CategoryCreate.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="col s12 m6">
+    <div>
+      <div class="page-subtitle">
+        <h4>Создать</h4>
+      </div>
+
+      <form  @submit.prevent="submitHandler">
+        <div class="input-field">
+          <input id="name" type="text" v-model="title" :class="{invalid: $v.title.$dirty && !$v.title.required}">
+          <label for="name">Название</label>
+          <span class="helper-text invalid"
+                v-if="$v.title.$dirty && !$v.title.required"
+          >Введите название</span>
+        </div>
+
+        <div class="input-field">
+          <input id="limit" type="number" v-model="limit" :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}">
+          <label for="limit">Лимит</label>
+          <span class="helper-text invalid"
+                v-if="$v.limit.$dirty && !$v.limit.minValue">
+            Минимальная величина {{$v.limit.$params.minValue.min}} </span>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit">
+          Создать
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+    import M from 'materialize-css'
+    import {required, minValue} from 'vuelidate/lib/validators'
+    export default {
+        name: "CategoryCreate",
+        data: () => ({
+          title: '',
+          limit: 10
+        }),
+      validations: {
+          title: {required},
+          limit: {minValue: minValue(10)}
+       },
+       mounted() {
+          M.updateTextFields()
+       },
+      methods: {
+       async submitHandler() {
+
+          if (this.$v.$invalid) {
+            this.$v.$touch()
+            return
+          }
+
+
+         }
+       }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
